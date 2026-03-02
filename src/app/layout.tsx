c1408f2b3d296d3a7002e2a09afaf34007851605
sepehrshapouri <sepehrshapouri9@gmail.com>
2026-03-02 15:57:32 +0300

    feat:styling changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
+import { Toaster } from "sileo";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
@@ -24,10 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${inter.variable} ${geistMono.variable} antialiased dark`}>
         {children}
+        <Toaster position="top-center" options={{styles:{description:'text-muted!'},roundness:12}}/>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <Button>
-        Click me
-      </Button>
+      <Button>Click me</Button>
     </div>
   );
 }
